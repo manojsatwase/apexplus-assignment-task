99b6234dea917693dbc2217f917f5dbd2eb28c82
manoj_s <apple@manojsatwase.local>
2024-05-21 16:57:48 +0530

    useToggle hook for select option

---
+++ frontend/src/hooks/useToggle.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+const useToggle = (initialValue = false) => {
+  const [value, setValue] = useState(initialValue);
+  const toggleValue = () => setValue((prevValue) => !prevValue);
+  return [value, toggleValue];
+};
+
+export default useToggle;
